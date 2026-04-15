4804522fbf79c9f7a32d2a7b5971120486a1b1b0
Santiago4566 <carrizosantiago64@gmail.com>
2026-04-15 19:02:27 -0300

    Interfaz Completada

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 void BuscarNombrePorID(char *nombre[])
 {
     int id;
-    printf("\nIngrese una ID para buscar");
+    printf("\nIngrese una ID para buscar :");
     scanf("%d",&id);
     if (id<1 || id >5)
     {
         printf("No se encontró el valor buscado");
     }else
     {
-     printf("\nNombre encontrado: %s",nombre[id-1]);   
+        printf("\nNombre encontrado: %s",nombre[id-1]);   
     }
-    
-}int BuscarNombrePorPalabra(char *nombres[],char *palabra)
+}
+int BuscarNombrePorPalabra(char *nombres[],char *palabra)
 {
     for (int i = 0; i < 5; i++)
     {
@@ -32,6 +33,41 @@ void MostrarPersonas(char *nombres[])
         printf(" \nNombre %d: %s",i+1,nombres[i]);
     }
     
+}
+void Interfaz(char *nombre[])
+{
+    int busqueda = 1;
+    do
+    {
+        int dato;
+        printf("\nSistema De busqueda");
+        printf("\nIngrese 1 para busqueda por ID, 2 para busqueda por Palabra :");
+        scanf("%d",&dato);
+        if (dato == 1)
+        {
+            (BuscarNombrePorID(nombre));
+        }else if (dato == 2)
+        {
+            char palabra[50];
+            int resultado;
+            printf("\ningrese una palabra para buscar: ");
+            scanf("%s",palabra);
+            resultado = BuscarNombrePorPalabra(nombre,palabra);
+            if (resultado!=-1)
+            {
+                printf("\nNombre encontrado: %s",nombre[resultado]);
+            }else
+            {
+                printf("\nNo se encontraron nombres");
+            }
+        }else
+        {
+            printf("\nnumero incorrecto");
+        }
+        printf("\nDesea Realisar otra Busqueda?");
+        printf("\n Si: 1 , No: otro numero : ");
+        scanf("%d",&busqueda);
+    } while (busqueda ==1);
 }
 void LiberarMemoria(char *nombres[])
 {
@@ -55,16 +91,6 @@ int main()
         strcpy(nombres[i],buff);
     }
     MostrarPersonas(nombres);
-    BuscarNombrePorID(nombres);
-    printf("\ningrese una palabra para buscar: ");
-    scanf("%s",palabra);
-    if (BuscarNombrePorPalabra(nombres,palabra)!=-1)
-    {
-        printf("\nNombre encontrado: %s",nombres[BuscarNombrePorPalabra(nombres,palabra)]);
-    }else
-    {
-        printf("No se encontraron nombres");
-    }
-    
+    Interfaz(nombres);
     LiberarMemoria(nombres);
 }
